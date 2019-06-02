95bdee028b3d97cc7e2c5fbafee9abeb5fd394b7
vic6595 <vic9565@hotmail.com>
2019-06-02 14:17:52 -0600

    non functional plot

--- python/plot.py
+++ python/plot.py
@@ -7,8 +7,8 @@ from parse import *
     groupBarPlot function takes: 
         arg title: title for the plot
         arg data:   matrix with data to be displayed on the bars. 
-                    A row contains all data for one group(same benchmark). 
-                    A column contains the same data of all groups 
+                    A column contains all data for one group(same benchmark). 
+                    A row contains the same data of all groups 
                     (Same value of parameter being tested
                     e.g Icache=512Kb).
         arg ylabel: y axis label (name of variable being displayed
@@ -30,6 +30,7 @@ def groupBarPlot(title, data, ylabel, groupLlabels, barLabels, colors):
     
     #drawing individual bars
     for col in data:
+        
         drawSubPlot(ax, ind, col, width, barLabels)
     
     #setting general plot labels
@@ -45,22 +46,6 @@ def drawSubPlot(ax, ind, dataVector, width, label):
 
 
 
-
-M64 = runParser("../../se_results/Queens_cache_line_size64/stats.txt")
-M128 = runParser("../../se_results/Queens_cache_line_size128/stats.txt")
-
-# M[numero de variable][1=dato, 0 = nombre largo de variable q no se usa] 
-print(M64[1][0])
-print(float(M64[1][1]))
-print('tags')
-print(getTags())
-
-a=['a','b','c']
-b=[1,2,3]
-
-for (z,x) in (a,b):
-    print(z)
-    print(x)
 
 groupBarPlot("sample plot", [float(M64[1][1]), float(M128[1][1])], "values", [M64[1][0], M128[1][0]],getTags(),2)
 
